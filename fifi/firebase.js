5682030d38bfb0ab46be57581b2269c464be4402
eunjaelim <imeunjae361@gmail.com>
2023-10-24 12:37:44 +0900

    init

---
+++ fifi/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCv4dZqNqiPegZahMX87kK4Kqch5oiRxn4",
+  authDomain: "fifi-7ceb0.firebaseapp.com",
+  projectId: "fifi-7ceb0",
+  storageBucket: "fifi-7ceb0.appspot.com",
+  messagingSenderId: "809958026748",
+  appId: "1:809958026748:web:e3b9044cd15a0ecbbb1679"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
